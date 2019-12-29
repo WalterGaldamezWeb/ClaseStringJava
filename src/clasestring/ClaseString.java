af16c69cf5565c7aa6e6ee8ae5dc348eeb89d211
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2019-12-29 08:56:49 -0600

    Clase String

---
+++ src/clasestring/ClaseString.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasestring;
+
+/**
+ *
+ * @author Walter Galdamez
+ */
+public class ClaseString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Clase String
+        //No es de tipo primitivo
+        //Es un objeto de tipo String y se puede declarar de la siguie te forma
+        
+        String mensaje = new String("Hola Programadores...");
+        
+        //Pero permite simplificar asi
+        
+        String mensajeDos = "Hola de nuevo...";
+        
+        System.out.print(mensaje);
+        System.out.println("");
+        System.out.println(mensajeDos);
+    }
+    
+}
